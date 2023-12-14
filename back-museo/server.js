14209708985831609b2f1c13a8d75f0e85d08249
javierommel <rommel_2211@hotmail.com>
2023-12-14 16:45:06 -0500

    Login y Register

--- back-museo/server.js
+++ back-museo/server.js
@@ -6,3 +6,8 @@ var corsOptions = { origin: "http://localhost:8081" };
 app.unsubscribe(cors(corsOptions))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+app.get("/", (req, res) => {
+    res.json({ message: "Bienvenido" });
+});
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => { console.log(`Server is running on port ${PORT}`) });
