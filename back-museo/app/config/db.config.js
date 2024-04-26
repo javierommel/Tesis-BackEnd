4ee48fe9017b7a9723fb0ecd796a832971207240
Rommel Chocho <rommel_2211@hotmail.com>
2024-04-25 23:27:35 -0500

    cambios generales

--- back-museo/app/config/db.config.js
+++ back-museo/app/config/db.config.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 
 module.exports = {
   HOST: process.env.HOST_DB,
+  PORT: process.env.PORT_DB,
   USER: process.env.USER_DB,
   PASSWORD: process.env.PASSWORD_DB,
   DB: process.env.DB,
